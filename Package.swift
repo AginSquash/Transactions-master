67a7f1860cd471b12d48432665ebd9fcb0e73d7f
AginS <agins.main@gmail.com>
2022-12-17 09:13:32 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  Transactions
+//
+//  Created by Anton Bronnikov on 30/05/2017.
+//  Copyright © 2017 Anton Bronnikov. All rights reserved.
+//
+
+import PackageDescription
+
+// swift-tools-version:5.0
+
+let package = Package(
+    name: "Transactions"
+)
